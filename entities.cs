491628887e426dd2bb2d0e4739217f7505664f42
julescavanier <jules.cavanier@ynov.com>
2022-09-21 11:36:20 +0200

    fixed rider

---
+++ entities.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Project_CS
+{
+    public class Entities
+    {
+        public Entities()
+        {
+            Console.WriteLine("I am the Hero");
+        }
+    }
+}
---
+++ stratCharacter.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Project_CS
+{
+    public class Hero: Entities
+    {
+        public override void Gon()
+        {
+            Console.WriteLine("I am the Hero");
+        }
+    }
+    
+}
