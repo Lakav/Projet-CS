2e9d0f5a50301f7ee96746e312e99e6fd427b35a
julescavanier <jules.cavanier@ynov.com>
2022-09-22 11:53:43 +0200

    dodge method all entities

--- Riehvelt.cs
+++ Riehvelt.cs
@@ -17,5 +17,21 @@ namespace Project_CS
             Console.WriteLine($"{Name} a une puissance de {Powerfull}, une esquive de {Dodge}, {Health} poits de vie et" +
                               $" un Nen a {Nen}");
         }
+        
+        public void ChDodge()
+        {
+            Console.WriteLine(Name);
+            Random dodge = new Random();
+            int chance = dodge.Next(0, 100);
+            
+            if (chance <= Dodge)
+            {
+                Console.WriteLine("You have dodged");
+            }
+            else if (chance > Dodge)
+            {
+                Console.WriteLine("You haven't dodged");
+            }
+        }
     }
 }
