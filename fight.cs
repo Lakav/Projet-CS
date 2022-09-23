a12d9e2c1eeab9c48092d28b650105f2f7b505af
Kschellens <97234163+Kschellens@users.noreply.github.com>
2022-09-23 10:09:44 +0200

    choix des pers

--- fight.cs
+++ fight.cs
@@ -10,16 +10,21 @@ namespace Project_CS
             {
                 Gon gon = new Gon();
                 gon.Character();
+                Console.WriteLine("Bienvenue Gon!");
             }
             if (Choice == "2")
             {
                 Kirua kirua = new Kirua();
                 kirua.Character();
+                Console.WriteLine("Bienvenue Kirua!");
+
             }
             if (Choice == "3")
             {
                 Zushi zushi = new Zushi();
                 zushi.Character();
+                Console.WriteLine("Bienvenue Zushi!");
+
             }
             Console.WriteLine("La selection n'est pas valide essayez a nouveau");
             CharacterChoice(Choice);
@@ -35,14 +40,23 @@ namespace Project_CS
                               "dizaine d'etages suivante." +
                               "Vous avez le droit de combattre uniquement les maitres d'etages pour monter par dizaines " +
                               "mais les combats seront plus hardus");
-            Console.ReadLine();
             
             Console.Clear();
 
             Console.WriteLine("Je vais commencer par prendre votre identiée, qui etes vous?");
+            Console.Clear();
+            Console.WriteLine("Pour choisir votre personnage choisissez respectivement | 1 pour Gon | 2 pour Kirua | 3 pour Zishi | \n"+
+                              "Gon : 1          Kirua               Zushi \n"+
+                              "Vie : 100        Vie : 100           Vie : 100 \n"+
+                              "Force : 12       Force : 9           Force : 9 \n"+
+                              "Esquive : 15     Esquive : 20        Esquive : 15 \n"+
+                              "Nen : 0          Nen : 0             Nen : 1 ");
 
-            Console.WriteLine("");
             String Choice = Console.ReadLine();
+
+            Console.WriteLine();
+
+            
             
 
 
