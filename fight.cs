a8d5b17599f727e891c33f09fec7124bb06b2224
julescavanier <jules.cavanier@ynov.com>
2022-09-23 11:00:01 +0200

    update

--- fight.cs
+++ fight.cs
@@ -4,30 +4,40 @@ namespace Project_CS
 {
     public class Battle
     {
-        public void CharacterChoice(string Choice)
+        public void CharacterChoice(string choice)
         {
-            if (Choice == "1")
+            if (choice == "1")
             {
+                Console.Clear();
+                
                 Gon gon = new Gon();
                 gon.Character();
-                Console.WriteLine("Bienvenue Gon!");
+                gon.printStats();
             }
-            if (Choice == "2")
+            if (choice == "2")
             {
+                Console.Clear();
+
                 Kirua kirua = new Kirua();
                 kirua.Character();
-                Console.WriteLine("Bienvenue Kirua!");
+                kirua.printStats();
 
             }
-            if (Choice == "3")
+            if (choice == "3")
             {
+                Console.Clear();
+
                 Zushi zushi = new Zushi();
                 zushi.Character();
-                Console.WriteLine("Bienvenue Zushi!");
+                zushi.printStats();
 
             }
-            Console.WriteLine("La selection n'est pas valide essayez a nouveau");
-            CharacterChoice(Choice);
+            else
+            {
+                Console.WriteLine("La selection n'est pas bonne.");
+                choice = Console.ReadLine();
+                CharacterChoice(choice);
+            }
         }
 
         
@@ -40,21 +50,24 @@ namespace Project_CS
                               "dizaine d'etages suivante." +
                               "Vous avez le droit de combattre uniquement les maitres d'etages pour monter par dizaines " +
                               "mais les combats seront plus hardus");
-            
+            Console.ReadLine();
             Console.Clear();
 
             Console.WriteLine("Je vais commencer par prendre votre identiée, qui etes vous?");
+            Console.ReadLine();
             Console.Clear();
-            Console.WriteLine("Pour choisir votre personnage choisissez respectivement | 1 pour Gon | 2 pour Kirua | 3 pour Zishi | \n"+
+            Console.WriteLine("Pour choisir votre personnage choisissez respectivement : 1 pour Gon | 2 pour Kirua | 3 pour Zishi | \n"+
                               "Gon : 1          Kirua               Zushi \n"+
-                              "Vie : 100        Vie : 100           Vie : 100 \n"+
+                              "Vie : 100        Vie : 100           Vie : 125 \n"+
                               "Force : 12       Force : 9           Force : 9 \n"+
                               "Esquive : 15     Esquive : 20        Esquive : 15 \n"+
                               "Nen : 0          Nen : 0             Nen : 1 ");
 
-            String Choice = Console.ReadLine();
+            String choice = Console.ReadLine();
 
-            Console.WriteLine();
+            CharacterChoice(choice);
+
+            Console.ReadLine();
 
             
             
